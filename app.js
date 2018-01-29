a6907fb2cf9dcbc867b7a6583fbc93c92cdc81b8
alw-AY's <ysc1217@gmail.com>
2018-01-29 18:59:42 +0800

    remove some files

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: alw-AY's
  * @Date:   2018-01-25 15:46:27
  * @Last Modified by:   alw-AY's
- * @Last Modified time: 2018-01-29 18:19:54
+ * @Last Modified time: 2018-01-29 18:58:51
  */
 
 var _ = require('lodash');
@@ -35,35 +35,12 @@ api.onReady(function() {
     "maxResults":100
   }, function(_data) {
 
-    var stream = fs.createWriteStream("my_file.txt");
-    stream.once('open', function(fd) {
-      _.forEach(_data,function(v){
-        console.log(v.length);
-        var ts = _.join(v,"∀");
-        ts = _.replace(ts, /\n\r/g, ' ');
-        ts = _.replace(ts, /\n/g, ' ');
-        ts = _.replace(ts, /\r/g, ' ');
-        stream.write(ts);
-        stream.write("\n\r");
-      })
-      stream.end();
-    });
-
-    // fs.writeFile("/tmp/test", _data, function(err) {
-    //     if(err) {
-    //         return console.log(err);
-    //     }
-
-    //     console.log("The file was saved!");
-    // });
 
 
-    // console.log(_data);
-
-    // _.forEach(_data,function(v){
-    //   console.log(v);
-    //   console.log("=============================");
-    // })
+    _.forEach(_data,function(v){
+      console.log(v);
+      console.log("\r\n");
+    })
   });
 
 })
